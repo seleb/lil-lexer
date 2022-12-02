e83ca1e7a7301890987f6862f79f981d16d8bd48
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-12-02 01:47:21 -0500

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import { babel } from '@rollup/plugin-babel';
 import pkg from './package.json';
 
 export default {
@@ -15,7 +15,7 @@ export default {
 	plugins: [
 		babel({
 			exclude: 'node_modules/**',
-			externalHelpers: false,
+			babelHelpers: 'bundled',
 		}),
 	],
 }
